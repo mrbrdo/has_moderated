794b1aece192133e3f08ef6380b0991366aa5e5a
MrBrdo <mrbrdo@gmail.com>
2011-09-08 05:14:27 +0200

    update moderation model generator

--- lib/generators/has_moderated/install/templates/moderation.rb
+++ lib/generators/has_moderated/install/templates/moderation.rb
@@ -2,8 +2,14 @@ class Moderation < ActiveRecord::Base
   belongs_to :moderatable, :polymorphic => true
 
   def accept
+    # case: moderated destruction
+    if attr_name == '-' && attr_value.class == String && attr_value == "destroy"
+      moderatable.has_moderated_updating = true
+      moderatable.destroy
+      moderatable.has_moderated_updating = false
+      self.destroy
     # case: moderated existance (new record)
-    if attr_name == '-'
+    elsif attr_name == '-'
       # create the main record
       rec = moderatable_type.constantize.new
       loaded_val = YAML::load(attr_value)
@@ -25,20 +31,25 @@ class Moderation < ActiveRecord::Base
 
         # all records for this associated model
         assoc_records.each do |attrs|
-          arec = m.new # new associated model
-          attrs.each_pair do |key, val|
-            arec.send(key.to_s+"=", val) unless key.to_s == 'id'
+          if attrs.class == Fixnum # associate to existing record
+            arec = m.find_by_id(attrs)
+            rec.send(assoc_name.to_s) << arec if arec # add the association, if the record still exists
+          else # create a new record
+            arec = m.new # new associated model
+            attrs.each_pair do |key, val|
+              arec.send(key.to_s+"=", val) unless key.to_s == 'id'
+            end
+            fk = if assoc_details.respond_to?(:foreign_key)
+              assoc_details.foreign_key
+            else # version < 3.1
+              assoc_details.primary_key_name
+            end
+            arec.send(fk.to_s+"=", rec.id) # set association to the newly created record
+            # disable moderation for associated model (if enabled)
+            arec.has_moderated_updating = true if arec.respond_to?("has_moderated_updating=")
+            arec.save
+            arec.has_moderated_updating = false if arec.respond_to?("has_moderated_updating=")
           end
-          fk = if assoc_details.respond_to?(:foreign_key)
-            assoc_details.foreign_key
-          else # version < 3.1
-            assoc_details.primary_key_name
-          end
-          arec.send(fk.to_s+"=", rec.id) # set association to the newly created record
-          # disable moderation for associated model (if enabled)
-          arec.has_moderated_updating = true if arec.respond_to?("has_moderated_updating=")
-          arec.save
-          arec.has_moderated_updating = false if arec.respond_to?("has_moderated_updating=")
         end
       end
       self.destroy # destroy this moderation since it has been applied
