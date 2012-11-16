bab9fd01d14985b3153fe19cdab9bd42f7e121ea
MrBrdo <mrbrdo@gmail.com>
2012-11-16 19:36:17 +0100

    add test for issue #18

--- spec/has_moderated/task_spec.rb
+++ spec/has_moderated/task_spec.rb
@@ -884,26 +884,32 @@ describe Task do
     end
   end
 
-  it "can handle nested associations" do
+  it "can handle nested associations (issue #17 and #18)" do
     reload_models.task {
       has_many :subtasks, :class_name => subtask_class_name, :foreign_key => task_fk
       has_moderated_create :with_associations => [:subtasks]
     }.subtask {
+      belongs_to :task_connection, :class_name => task_connection_class_name, :foreign_key => "parentable_id"
       belongs_to :task, :class_name => task_class_name
       has_many :photos, :as => :parentable, :class_name => photo_class_name
+      has_moderated :title
     }.photo {
       belongs_to :parentable, :polymorphic => true
+    }.task_connection {
+      has_many :things, :as => :parentable
     }
 
+    tconn = TaskConnection.create! :title => "TC"
     data = {
       :create => {
-        :attributes => { "title" => "Task" },
+        :attributes => { "id" => nil, "title" => "Task" },
         :associations => {
           :subtasks => 
             [
-              {"title" => "Subtask",
+              {"id" => nil, "title" => "Subtask",
                 :associations => {
-                  :photos => [{"title" => "Photo"}]
+                  :photos => [{"id" => nil, "title" => "Photo"}],
+                  :task_connection => [tconn.id]
                 }
               }
             ]
@@ -912,8 +918,10 @@ describe Task do
     }
     Moderation.create! :moderatable_type => "Task", :data => data.to_yaml
     Moderation.last.accept
+    Moderation.count.should eq(0)
     (t = Task.last).title.should eq("Task")
     (s = t.subtasks.last).title.should eq("Subtask")
     s.photos.last.title.should eq("Photo")
+    s.task_connection.title.should eq("TC")
   end
 end
